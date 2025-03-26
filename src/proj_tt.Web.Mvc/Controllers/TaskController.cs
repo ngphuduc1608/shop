9d6ad00c6faaede595b34d07838d873ab757f2ed
NGUYỄN PHÚ ĐỨC <2121050654@student.humg.edu.vn>
2025-03-26 15:20:42 +0700

    fix

---
+++ src/proj_tt.Web.Mvc/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using proj_tt.Tasks.Dto;
+using proj_tt.Tasks;
+using System.Threading.Tasks;
+using proj_tt.Controllers;
+using proj_tt.Web.Models.Tasks;
+
+namespace proj_tt.Web.Controllers
+{
+    public class TasksController : proj_ttControllerBase
+    {
+        private readonly ITaskAppService _taskAppService;
+
+        public TasksController(ITaskAppService taskAppService)
+        {
+            _taskAppService = taskAppService;
+        }
+
+        public async Task<ActionResult> Index(GetAllTasksInput input)
+        {
+            var output = await _taskAppService.GetAll(input);
+            var model = new IndexViewModel(output.Items);
+            return View(model);
+        }
+    }
+}
---
+++ src/proj_tt.Web.Mvc/Models/Tasks/IndexViewModel.cs
@@ -0,0 +1,27 @@
+﻿using proj_tt.Tasks.Dto;
+using proj_tt.Tasks;
+using System.Collections.Generic;
+
+namespace proj_tt.Web.Models.Tasks
+{
+    public class IndexViewModel
+    {
+        public IReadOnlyList<TaskListDto> Tasks { get; }
+
+        public IndexViewModel(IReadOnlyList<TaskListDto> tasks)
+        {
+            Tasks = tasks;
+        }
+
+        public string GetTaskLabel(TaskListDto task)
+        {
+            switch (task.State)
+            {
+                case TaskState.Open:
+                    return "label-success";
+                default:
+                    return "label-default";
+            }
+        }
+    }
+}
