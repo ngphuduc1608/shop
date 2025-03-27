f9862f6bc5c19f1f5dceacdcc7a830152157a813
NGUYỄN PHÚ ĐỨC <2121050654@student.humg.edu.vn>
2025-03-27 09:37:33 +0700

    part2

--- src/proj_tt.Web.Mvc/Controllers/TaskController.cs
+++ src/proj_tt.Web.Mvc/Controllers/TaskController.cs
@@ -4,16 +4,25 @@ using proj_tt.Tasks;
 using System.Threading.Tasks;
 using proj_tt.Controllers;
 using proj_tt.Web.Models.Tasks;
+using proj_tt.Persons;
+using System.Linq;
+using Abp.Application.Services.Dto;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using proj_tt.Web.Models.Persons;
 
 namespace proj_tt.Web.Controllers
 {
     public class TasksController : proj_ttControllerBase
     {
         private readonly ITaskAppService _taskAppService;
+        private readonly ILookupAppService _lookupAppService;
 
-        public TasksController(ITaskAppService taskAppService)
+
+        public TasksController(ITaskAppService taskAppService, ILookupAppService lookupAppService)
         {
             _taskAppService = taskAppService;
+            _lookupAppService = lookupAppService;
+
         }
 
         public async Task<ActionResult> Index(GetAllTasksInput input)
@@ -24,6 +33,16 @@ namespace proj_tt.Web.Controllers
                 SelectedTaskState=input.State
             };
             return View(model);
+        }
+
+        public async Task<ActionResult> Create()
+        {
+            var peopleSelectListItems=(await _lookupAppService.GetPeopleComboboxItems()).Items
+                .Select(p=>p.ToSelectListItem()).ToList();
+
+            peopleSelectListItems.Insert(0,new SelectListItem { Value=string.Empty,Text="Unassigned",Selected=true});
+
+            return View(new CreateTaskViewModel(peopleSelectListItems));
         }
     }
 }
